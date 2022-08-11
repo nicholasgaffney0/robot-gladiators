ff7f8c39ea2e4795ef77413301d974faf9de28d1
Nick Gaffney <nicholasgaffney0@gmail.com>
2022-08-11 14:29:34 -0400

    updated rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -93,9 +93,4 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
-<<<<<<< HEAD
 }
-=======
-}
-hello 
->>>>>>> feature/initial-game
